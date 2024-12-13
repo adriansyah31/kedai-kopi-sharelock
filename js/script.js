9e27749c63ede6410b61450af17c929f4dbed847
Adriansyah <aadry793@gmail.com>
2024-12-13 14:57:27 +0800

    menambahkan css making

--- js/script.js
+++ js/script.js
@@ -1,15 +1,65 @@
-// Toogle class Active
-const navbarNav = document.querySelector('.navbar-nav');
+// Toogle class active untuk hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
 // Ketika hamburger menu di click
-document.querySelector('#hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+document.querySelector("#hamburger-menu").onclick = (e) => {
+  navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// Click di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// Toogle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
-document.addEventListener('click', function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove('active');
-    }
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toogle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Click di luar elemen untuk menghilangkan nav
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    itemDetailModal.style.display = "flex";
+  });
+});
+
+const closeIcon = document.querySelector(".close-icon");
+closeIcon.addEventListener("click", (e) => {
+  e.preventDefault();
+  itemDetailModal.style.display = "none";
+});
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    modal.style.display = "none";
+  }
+};
